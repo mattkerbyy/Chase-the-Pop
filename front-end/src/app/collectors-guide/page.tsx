7e8bb653c88ace6b35074939ca115598bd1be662
Kerbytes <mattkerbyy@gmail.com>
2025-07-31 23:54:50 +0800

    Modified Collector's Guide (page.tsx and layout.tsx) as a Reminder v2

--- front-end/src/app/collectors-guide/page.tsx
+++ front-end/src/app/collectors-guide/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CollectorsGuide() {
-  return <div>Collector&apos;s Guide: COMING SOON</div>;
+  return <div>Collector&apos;s Guide: COMING SOON!</div>;
 }
 
 export default CollectorsGuide;
